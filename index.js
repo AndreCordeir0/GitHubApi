5b54396deae7c5305a6d212ed1e51f175fb313bd
André Cordeiro <93953859+AndreCordeir0@users.noreply.github.com>
2022-05-18 09:19:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ let link =document.querySelector('#link');
 let twitter =document.querySelector('#twitter');
 let twitter2 =document.querySelector('.twitter');
 let img =document.querySelector('#img');
-
-
+let dark = document.querySelector('.dark');
+dark.addEventListener('click',()=>{
+    document.body.classList.toggle('light')
+})
 former.addEventListener('submit',(event)=>{
 event.preventDefault();
 
@@ -32,22 +34,19 @@ async function pegarDados(nome){
            return res.json()
         })
         transfereDados(dados)
-        console.log(dados.statusCode);
+
        
-        
     }catch(err){
         console.error(err)
         alert("Usuario invalido")
     }
    
 }
+
 function transfereDados(github){
 
-   
         if(statusDaApi ===200){
 
-        
-    
 nickMaior.innerHTML= github.login
 nickMenor.innerHTML= github.name
 bio.innerHTML= github.bio
@@ -58,13 +57,64 @@ followers.innerHTML= github.followers
 following.innerHTML= github.following
 link.innerHTML= github.html_url
 twitter2.innerHTML= github.twitter_username
-data.innerHTML = `Joined ${github.created_at.slice(0,10)}`
-console.log("oi"+data.innerHTML);
-img.src= `${github.html_url}.png`
-console.log(twitter.innerHTML);
+
+let mes = github.created_at.slice(5,7)
+let mensagem;
+switch(mes) {
+    case "01":
+        mensagem ="Jan"
+        break;
+    case "02":
+        mensagem ="Feb"
+        break;
+    case "03":
+        mensagem ="Mar"
+        break;
+    case "04":
+        mensagem ="Apr"
+        break;
+    case "05":
+        mensagem ="May"
+        break;
+    case "06":
+        mensagem ="June"
+        break;
+    case "07":
+        mensagem ="Jul"
+        break;
+    case "08":
+        mensagem ="Aug"
+        break;
+    case "09":
+        mensagem ="Sept"
+        break;
+    case "10":
+        mensagem ="Oct"
+        break;
+    case "11":
+        mensagem = "Nov";
+        break;
+    case "12":
+        mensagem = "Dec";
+        break;
+}
+console.log(mes);
+data.innerHTML = `Joined ${github.created_at.slice(8,10)} ${mensagem} ${github.created_at.slice(0,4)}`
+
+if(inputValor.length <= 3){
+    img.src ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn11Rru16VFClaKhYzoL9yGLIwOEaggMpa1Q&usqp=CAU'
+    }else{
+        img.src= `${github.html_url}.png`
+    }
+
+
 
 if(bio.innerHTML === ""){
     bio.innerHTML= "This profile has no bio"
+}
+if(localization.innerHTML === ""){
+
+    localization.innerHTML= "This profile has no localization"
 }
 if(twitter2.innerHTML == ""){
     twitter2.innerHTML = "not Avaliable"
